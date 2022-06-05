706a883bbca0536b6554601740f2b82429027394
N3M35I5 <adwaith1998kv@gmail.com>
2022-06-05 13:41:53 +0530

    Added Styling to btns and basic JS

--- index.js
+++ index.js
@@ -18,3 +18,116 @@ const divide = function(a,b)
     }
     return a/b;
 }
+
+const operate = function(a,b)
+{
+    let Symbol='/';
+    if(Symbol=='+')
+    {
+        console.log(add(a,b));
+    }
+    else if(Symbol=='-')
+    {
+        console.log(subtract(a,b));
+    }
+    else if(Symbol=='*')
+    {
+        console.log(multiply(a,b));
+    }
+    else if(Symbol=='/')
+    {
+        console.log(divide(a,b));
+    }
+}
+operate(1,2);
+
+let miniScreenVal='';
+let mainScreenVal='';
+const setMiniScreen = function(miniScreenVal)
+{
+    const miniScreen = document.querySelector('#calcMiniScreen');
+    miniScreen.textContent = miniScreenVal;
+}
+const setMainScreen = function(mainScreenVal)
+{
+    const mainScreen = document.querySelector('#calcMainScreen');
+    mainScreen.textContent = mainScreenVal;
+}
+
+const numVal = function(e)
+{
+    let val = Number(e.target.innerText);
+    mainScreenVal+=val;
+    setMainScreen(mainScreenVal);
+}
+const symVal = function(e)
+{
+    let val = e.target.innerText;
+    miniScreenVal+=mainScreenVal+val;
+    setMiniScreen(miniScreenVal);
+    mainScreenVal='';
+    setMainScreen(mainScreenVal);
+}
+
+let a = null;
+let b = null;
+
+//7
+const seven = document.querySelector('#seven');
+seven.addEventListener('click',numVal);
+//8
+const eight = document.querySelector('#eight');
+eight.addEventListener('click',numVal);
+//9
+const nine = document.querySelector('#nine');
+nine.addEventListener('click',numVal);
+///
+const division = document.querySelector('#division');
+division.addEventListener('click',symVal);
+
+//4
+const four = document.querySelector('#four');
+four.addEventListener('click',numVal);
+//5
+const five = document.querySelector('#five');
+five.addEventListener('click',numVal);
+//6
+const six = document.querySelector('#six');
+six.addEventListener('click',numVal);
+//*
+const mul = document.querySelector('#mul');
+mul.addEventListener('click',symVal);
+
+//1
+const one = document.querySelector('#one');
+one.addEventListener('click',numVal);
+//2
+const two = document.querySelector('#two');
+two.addEventListener('click',numVal);
+//3
+const three = document.querySelector('#three');
+three.addEventListener('click',numVal);
+//-
+const minus = document.querySelector('#minus');
+minus.addEventListener('click',symVal);
+
+//.
+const dot = document.querySelector('#dot');
+dot.addEventListener('click',symVal);
+//0
+const zero = document.querySelector('#zero');
+zero.addEventListener('click',numVal);
+//=
+const equal = document.querySelector('#equal');
+equal.addEventListener('click',symVal);
+//+
+const plus = document.querySelector('#plus');
+plus.addEventListener('click',symVal);
+
+//clear
+const clear = document.querySelector('#clear');
+clear.addEventListener('click',()=>{
+    miniScreenVal='';
+    setMiniScreen(miniScreenVal);
+    setMainScreen(0);
+});
